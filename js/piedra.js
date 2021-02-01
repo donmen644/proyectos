f6c86cbd9856099677f8060df49cc6c9135618c7
Mr Darkes <63811789+donmen644@users.noreply.github.com>
2021-01-31 18:38:56 -0600

    pruebas locas

---
+++ js/piedra.js
@@ -0,0 +1,69 @@
+var piedra = 0;
+var papel = 1;
+var tijera = 2;
+
+var opcionUsuario;
+var opcionMaquina;
+
+opcionUsuario = prompt("Que eliges?\nPiedra: 0\nPapel: 1\nTijera: 2", 0);
+
+var opciones = ["Piedra", "Papel", "Tijera"];
+
+var opcionUsuario;
+var opcionMaquina = patito(0,2);
+
+opcionUsuario = prompt("¿Qué eliges?\nPiedra: 0\nPapel: 1\nTijera: 2", 0);
+
+alert("Elegiste " + opciones[opcionUsuario]);
+alert("Javascript eligió " + opciones[opcionMaquina]);
+
+if(opcionUsuario == piedra)
+{
+    if(opcionMaquina == piedra)
+    {
+        alert("Empate!");
+    }
+    else if(opcionMaquina == papel)
+    {
+        alert("Perdiste");
+    }
+    else if(opcionMaquina == tijera)
+    {
+        alert("Ganaste!");
+    }
+}
+else if(opcionUsuario == papel)
+{
+    if(opcionMaquina == piedra)
+    {
+        alert("Ganaste!");
+    }
+    else if(opcionMaquina == papel)
+    {
+        alert("Empate!");
+    }
+    else if(opcionMaquina == tijera)
+    {
+        alert("Perdiste!");
+    }
+}
+else if(opcionUsuario == tijera)
+{
+    if(opcionMaquina == piedra)
+    {
+        alert("Perdiste!");
+    }
+    else if(opcionMaquina == papel)
+    {
+        alert("Ganaste!");
+    }
+    else if(opcionMaquina == tijera)
+    {
+        alert("Empate!");
+    }
+}
+else
+{
+    alert("¿Pos qué carajo?");
+}
+
